25a8bfbe203c8c868279c0fa82451db25b1817db
umakantchauhan <140892351+umakantchauhan@users.noreply.github.com>
2025-12-18 17:27:22 +0530

    connecting with google generativd AI for chating

--- react_AI_chatbot/src/App.jsx
+++ react_AI_chatbot/src/App.jsx
@@ -2,12 +2,31 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const googleai = new GoogleGenerativeAI(import.meta.enc.VITE_GOOGLE_API_KEY);
+const gemini = googleai.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = gemini.startChat({ history: [] });
 
 function App() {
   const [messages, setMessages] = useState([]);
 
-  function handleContentSend(content) {
-    setMessages((prevMessages) => [...prevMessages, { content, role: "user" }]);
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(), role: "assistant" });
+    } catch (error) {
+      console.error("Error sending message to AI:", error);
+      addMessage({
+        content: "Sorry I couldn't process your request. Please try again",
+        role: "system",
+      });
+    }
   }
 
   return (
