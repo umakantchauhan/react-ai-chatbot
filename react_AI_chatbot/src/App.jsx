863ee7265e528f155b8527b2750169df41c30207
umakantchauhan <140892351+umakantchauhan@users.noreply.github.com>
2025-12-19 20:27:57 +0530

    added loader CSS while the API call goes in the back

--- react_AI_chatbot/src/App.jsx
+++ react_AI_chatbot/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
-// import { Assistant } from "./assistants/googleai";
-import { Assistant } from "./assistants/openai";
+import { Assistant } from "./assistants/googleai";
+// import { Assistant } from "./assistants/openai";
+import { Loader } from "./components/Loader/Loader";
 
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -15,6 +17,7 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
+    setIsLoading(true);
     try {
       const result = await assistant.chat(content, messages);
       addMessage({ content: result, role: "assistant" });
@@ -24,12 +27,15 @@ function App() {
         content: "Sorry I couldn't process your request. Please try again",
         role: "system",
       });
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
     <>
       <div className={styles.App}>
+        {isLoading && <Loader />}
         <header className={styles.Header}>
           <img className={styles.Logo} src="/chatbot.png" alt="" />
           <h2 className={styles.Title}>AI chatbot</h2>
